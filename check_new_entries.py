80602927f9eef52610ece3cc6545a2224cbf4cc7
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-02-25 13:01:12 -0500

    Minor changes + bug fix

--- check_new_entries.py
+++ check_new_entries.py
@@ -1,7 +1,7 @@
 import argparse,sys,os,requests
 
 parser = argparse.ArgumentParser()
-parser.add_argument("url","The URL to download the allowlist from")
+parser.add_argument("url",help="The URL to download the allowlist from")
 parser.add_argument("--file", help="A file to check against the allowlist")
 parser.add_argument("--domain", help="A domain to check against the allowlist")
 args = parser.parse_args()
@@ -9,16 +9,17 @@ args = parser.parse_args()
 domainstocheck = []
 
 if args.file:
-  filedomains = open(args.file).read().split("\n")
-  for d in filedomains:
-    if d == "" or d.startswith("#") or d.startswith("!"):
-      continue
-    domainstocheck.append(d)
+	filedomains = open(args.file).read().split("\n")
+	for d in filedomains:
+		if d == "" or d.startswith("#") or d.startswith("!"):
+			continue
+		domainstocheck.append(d)
 if args.domain:
-  domainstocheck.append(args.domain)
+	domainstocheck.append(args.domain)
 
 allowlist = requests.get(args.url).text.split("\n")
 for d in domainstocheck:
-  if d in allowlist:
-    print(f"[in allowlist] {d}")
-    
+	if d in allowlist:
+		print(f"[in allowlist] {d}")
+	else:
+		print(f"[not in allowlist] {d}")
