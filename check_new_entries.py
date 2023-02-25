00205e5b5db09c4c118af1cad3086c59ca0e6179
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-02-25 12:55:05 -0500

    Create a Python script to make my life easier

---
+++ check_new_entries.py
@@ -0,0 +1,24 @@
+import argparse,sys,os,requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument("url","The URL to download the allowlist from")
+parser.add_argument("--file", help="A file to check against the allowlist")
+parser.add_argument("--domain", help="A domain to check against the allowlist")
+args = parser.parse_args()
+
+domainstocheck = []
+
+if args.file:
+  filedomains = open(args.file).read().split("\n")
+  for d in filedomains:
+    if d == "" or d.startswith("#") or d.startswith("!"):
+      continue
+    domainstocheck.append(d)
+if args.domain:
+  domainstocheck.append(args.domain)
+
+allowlist = requests.get(args.url).text.split("\n")
+for d in domainstocheck:
+  if d in allowlist:
+    print(f"[in allowlist] {d}")
+    
